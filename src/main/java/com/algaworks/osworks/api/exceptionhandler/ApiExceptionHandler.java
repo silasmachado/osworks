2c205b72e257e48f915cb94f779c5626cdbf4caa
Silas Machado <silas.machado.souza@everis.com>
2020-04-26 17:45:32 -0300

    Projeto Algaworks Ordem de Serviço - endpoints CRUD e exceções de negócio implementados

---
+++ src/main/java/com/algaworks/osworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,58 @@
+package com.algaworks.osworks.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.algaworks.osworks.domain.exception.NegocioException;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<Object> handleNegocioException(NegocioException ex, WebRequest request) {
+		var status = HttpStatus.BAD_REQUEST;
+		
+		var problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setTitulo(ex.getMessage());
+		problema.setDataHora(LocalDateTime.now());
+		
+		return handleExceptionInternal(ex, problema, new HttpHeaders(), status, request);
+		
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		var campos = new ArrayList<Campo>();
+		
+		for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+
+			String nomeCampo = ((FieldError) error).getField();
+			String mensagem = error.getDefaultMessage();
+			campos.add(new Campo(nomeCampo, mensagem));
+		}
+		
+		var problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setTitulo("Um ou mais campos estão inválidos. " 
+				+ "Faça o preenchimento correto e tente novamente");
+		problema.setDataHora(LocalDateTime.now());
+		problema.setCampos(campos);
+		
+		return super.handleExceptionInternal(ex, problema, headers, status, request);
+	}
+
+}
---
+++ src/main/java/com/algaworks/osworks/domain/service/CadastroClienteService.java
@@ -0,0 +1,30 @@
+package com.algaworks.osworks.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.osworks.domain.exception.NegocioException;
+import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
+
+@Service
+public class CadastroClienteService {
+	
+	@Autowired
+	private ClienteRepository repository;
+	
+	public Cliente salvar(Cliente cliente) {
+		Cliente clienteExistente = repository.findByEmail(cliente.getEmail());
+		
+		if (clienteExistente != null && !clienteExistente.equals(cliente)) {
+			throw new NegocioException("Já existe um cliente cadastrado com este e-mail");
+		}
+		
+		return repository.save(cliente);
+	}
+	
+	public void excluir(Long clienteId) {
+		repository.deleteById(clienteId);
+	}
+
+}
